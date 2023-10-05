e2af07dfbd208c903481d28d8c05daf6ded6e874
jocaher0503 <jocaher0503@g.educaand.es>
2023-10-05 09:52:17 +0200

    05/10/2023

---
+++ CAP02EJ01.java
@@ -0,0 +1,11 @@
+public class CAP02EJ01 {
+    public static void main(String[] args) {
+        int x =144, y=999;
+        System.out.println("x= "+x);
+        System.out.println("y= "+y);
+        System.out.println("x+y= "+(x+y));
+        System.out.println("x-y= "+(x-y));
+        System.out.println("x/y= "+((float)x/(float)y));
+        System.out.println("x*y= "+(x*y));
+    }
+}
---
+++ CAP02EJ08.java
@@ -0,0 +1,12 @@
+public class CAP02EJ08 {
+    public static void main(String[] args) {
+        char letra1, letra2, letra3, letra4, letra5;
+        letra1='s';
+        letra2='a';
+        letra3='l';
+        letra4='u';
+        letra5='d';
+        String cadena=""+letra1+letra2+letra3+letra4+letra5;
+        System.out.println(cadena);
+    }
+}
